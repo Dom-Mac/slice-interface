8390192df967fef283977aaff8d626c8024f57bf
Dom-Mac <domenicochad@gmail.com>
2022-07-17 19:04:27 +0200

    code cleaned

--- utils/saEvent.ts
+++ utils/saEvent.ts
@@ -1,6 +1,5 @@
 export default function saEvent(eventName: string) {
   if (navigator.doNotTrack !== "1") {
-    // TODO: uncomment this line when the service is ready
-    // sa_event(eventName)
+    sa_event(eventName)
   }
 }
