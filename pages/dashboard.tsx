20050039849f51a1ed70275d0350066cc0982ced
Dom-Mac <domenicochad@gmail.com>
2022-08-06 23:45:39 +0200

    temp checkout

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,8 +7,39 @@ import {
   domain
 } from "@components/common/Head"
 import VisibilityOpen from "@components/icons/VisibilityOpen"
+import { useAppContext } from "@components/ui/context"
+import useQuery from "@utils/subgraphQuery"
 
-export default function Profile() {
+export default function Dashboard() {
+  const { account } = useAppContext()
+  console.log(account)
+
+  const tokensQuery = /* GraphQL */ `
+      payee(id: "${account?.toLowerCase()}") {
+        slicers (where: {slices_gt: "0"}){
+          slices
+          slicer {
+            id
+            address
+            slices
+            minimumSlices
+            isImmutable
+            productsModuleBalance
+            protocolFee
+          }
+        }
+        # currencies(where: {toWithdraw_gt: "1"}){
+        #   toWithdraw
+        #   currency {
+        #     id
+        #   }
+        # }
+      }
+    `
+  let subgraphData = useQuery(tokensQuery, [account])
+
+  console.log(subgraphData)
+
   return (
     <Container page={true}>
       <NextSeo
@@ -36,15 +67,15 @@ export default function Profile() {
             <p className="mr-3 text-lg ">My balance</p>
             <VisibilityOpen className="h-6" />
           </div>
-          <div className="flex w-3/4 p-2 rounded-lg bg-slate-800 dark:bg-slate-800">
-            <div className="w-1/2 text-left">
+          <div className="flex justify-between w-3/5 p-2 rounded-lg min-w-fit bg-slate-800 dark:bg-slate-800">
+            <div className="text-left ">
               <p className="text-xs font-normal text-slate-400">Total earned</p>
               <p className="text-lg font-semibold">$ 999.99</p>
               <p className="text-xs font-normal text-green-500">
                 +200 SLX cashback
               </p>
             </div>
-            <div className="w-1/2 text-left">
+            <div className="text-left ">
               <p className="text-xs font-normal text-slate-400">To withdraw</p>
               <p className="text-lg font-semibold">$ 2345.67</p>
               <p className="text-xs font-normal text-green-500">+200 tokens</p>
