fb5f60a0b7f1405efd9169022d65dcd3d0d87645
Dom-Mac <domenicochad@gmail.com>
2022-08-06 22:52:04 +0200

    total balance fe

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,6 +6,7 @@ import {
   longTitle,
   domain
 } from "@components/common/Head"
+import VisibilityOpen from "@components/icons/VisibilityOpen"
 
 export default function Profile() {
   return (
@@ -28,7 +29,27 @@ export default function Profile() {
       />
       <ConnectBlock>
         <main className="max-w-[420px] mx-auto sm:max-w-screen-md">
-          <h1 className="text-2xl font-normal text-left">Earnings Dashboard</h1>
+          <h1 className="mb-6 text-2xl font-normal text-left">
+            Earnings Dashboard
+          </h1>
+          <div className="flex items-center mb-4">
+            <p className="mr-3 text-lg ">My balance</p>
+            <VisibilityOpen className="h-6" />
+          </div>
+          <div className="flex w-3/4 p-2 rounded-lg bg-slate-800 dark:bg-slate-800">
+            <div className="w-1/2 text-left">
+              <p className="text-xs font-normal text-slate-400">Total earned</p>
+              <p className="text-lg font-semibold">$ 999.99</p>
+              <p className="text-xs font-normal text-green-500">
+                +200 SLX cashback
+              </p>
+            </div>
+            <div className="w-1/2 text-left">
+              <p className="text-xs font-normal text-slate-400">To withdraw</p>
+              <p className="text-lg font-semibold">$ 2345.67</p>
+              <p className="text-xs font-normal text-green-500">+200 tokens</p>
+            </div>
+          </div>
         </main>
       </ConnectBlock>
     </Container>
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,6 +41,18 @@ module.exports = {
         gray: colors.gray,
         sky: colors.sky,
         cyan: colors.cyan,
+        slate: {
+          50: "#f8fafc",
+          100: "#f1f5f9",
+          200: "#e2e8f0",
+          300: "#cbd5e1",
+          400: "#94a3b8",
+          500: "#64748b",
+          600: "#475569",
+          700: "#334155",
+          800: "#1e293b",
+          900: "#0f172a"
+        },
         random1: {
           300: "var(--color1)",
           600: "var(--darkColor1)"
