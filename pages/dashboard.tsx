32ba61207ac2031268b4cfb12752d41f5a3856e7
Dom-Mac <domenicochad@gmail.com>
2022-08-06 19:40:53 +0200

    empty dashboard page

---
+++ pages/dashboard.tsx
@@ -0,0 +1,34 @@
+import { ConnectBlock, Container } from "@components/ui"
+import { NextSeo } from "next-seo"
+import {
+  defaultDescription,
+  defaultTitle,
+  longTitle,
+  domain
+} from "@components/common/Head"
+
+export default function Profile() {
+  return (
+    <Container page={true}>
+      <NextSeo
+        title="Your slicers"
+        openGraph={{
+          title: longTitle,
+          description: defaultDescription,
+          url: domain,
+          images: [
+            {
+              url: `${domain}/og_image.jpg`,
+              width: 1000,
+              height: 1000,
+              alt: `${defaultTitle} cover image`
+            }
+          ]
+        }}
+      />
+      <ConnectBlock>
+        <main className="max-w-[420px] mx-auto sm:max-w-screen-md"></main>
+      </ConnectBlock>
+    </Container>
+  )
+}
