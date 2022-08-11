86fee35e9fa05c02099cbb8ec20247ad6bb80f09
Dom-Mac <domenicochad@gmail.com>
2022-08-11 17:58:26 +0200

    complete refactor, data taken from subgraph

--- components/ui/TotalBalance/TotalBalance.tsx
+++ components/ui/TotalBalance/TotalBalance.tsx
@@ -1,11 +1,25 @@
 import VisibilityOpen from "@components/icons/VisibilityOpen"
+import { ethers } from "ethers"
 
 type Props = {
-  totalEarnedUsd: string
-  toWithdrawUsd: string
+  currencies: any[]
+  ethUsd: any
 }
 
-const TotalBalance = ({ totalEarnedUsd, toWithdrawUsd }: Props) => {
+const TotalBalance = ({ currencies, ethUsd }) => {
+  const addrO = ethers.constants.AddressZero
+  const ethBalance = currencies?.filter((c) => c.id.split("-")[1] == addrO)[0]
+  const withdrawnEth = ethers.utils.formatEther(ethBalance?.withdrawn || 0)
+  const toWithdrawEth = ethers.utils.formatEther(ethBalance?.toWithdraw || 0)
+  const totalEarnedEth = Number(withdrawnEth) + Number(toWithdrawEth)
+  // Conversion in USD
+  const totalEarnedUsd = (totalEarnedEth * Number(ethUsd?.price)).toFixed(2)
+  const toWithdrawUsd = (Number(toWithdrawEth) * Number(ethUsd?.price)).toFixed(
+    2
+  )
+  // const otherTokens = currencies?.filter((c) => c.id.split("-")[1] != addrO)
+  const otherTokens = [1]
+
   return (
     <>
       <div className="flex items-center mb-4">
@@ -23,7 +37,11 @@ const TotalBalance = ({ totalEarnedUsd, toWithdrawUsd }: Props) => {
         <div className="text-left ">
           <p className="text-xs font-normal text-slate-400">To withdraw</p>
           <p className="text-lg font-semibold">$ {toWithdrawUsd}</p>
-          {/* <p className="text-xs font-normal text-green-500">+200 tokens</p> */}
+          {otherTokens.length > 0 && (
+            <p className="text-xs font-normal text-green-500">
+              +{otherTokens.length} token{otherTokens.length > 1 ? "s" : null}
+            </p>
+          )}
         </div>
       </div>
       <p className="p-2 mb-5 text-xs font-normal text-left text-slate-500 ">
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -13,15 +13,11 @@ import {
 } from "@components/common/Head"
 import { useAppContext } from "@components/ui/context"
 import useQuery from "@utils/subgraphQuery"
-import useUnreleased from "@lib/useUnreleased"
-import { ethers } from "ethers"
-import getEthFromWei from "@utils/getEthFromWei"
 import useSWR from "swr"
 import fetcher from "@utils/fetcher"
 
 export default function Dashboard() {
   const { account } = useAppContext()
-  const addrO = ethers.constants.AddressZero
   const { data: ethUsd } = useSWR(
     "https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT",
     fetcher
@@ -29,44 +25,18 @@ export default function Dashboard() {
 
   const tokensQuery = /* GraphQL */ `
       payee(id: "${account?.toLowerCase()}") {
-        slicers (where: {slices_gt: "0"}){
-          slicer {
-            id
-            address
-          }
-        }
         currencies(where: {toWithdraw_gt: "0"}) {
           id
           withdrawn
+          toWithdraw
+          toPayToProtocol
+          paidToProtocol
         }
       }
     `
   let subgraphData = useQuery(tokensQuery, [account])
   const payee = subgraphData?.payee
-  const slicers = payee?.slicers
   const currencies = payee?.currencies
-  const currenciesAddresses = currencies?.map((c) => c.id.split("-")[1])
-
-  // Unreleased amounts are taken from blockchain
-  // TODO: divide useUnreleased by currencies
-  // Are all the currencies in the array the same? Right now there is only ETH
-  const unreleased = useUnreleased(slicers, account, currenciesAddresses || [])
-  // Amount already withdrawn from the payee
-  const withdrawnEth = currencies?.filter((c) => c.id.split("-")[1] == addrO)[0]
-    ?.withdrawn
-  const toWithdrawEth = unreleased.reduce((acc, curr) => {
-    return acc + getEthFromWei(curr, true)
-  }, 0)
-  const totalEarnedEth =
-    Number(ethers.utils.formatEther(withdrawnEth || 0)) + Number(toWithdrawEth)
-
-  // Conversion in USD
-  const totalEarnedUsd = (
-    Number(totalEarnedEth) * Number(ethUsd?.price)
-  ).toFixed(2)
-  const toWithdrawUsd = (Number(toWithdrawEth) * Number(ethUsd?.price)).toFixed(
-    2
-  )
 
   return (
     <Container page={true}>
@@ -91,16 +61,13 @@ export default function Dashboard() {
           <h1 className="mb-6 text-2xl font-normal text-left">
             Earnings Dashboard
           </h1>
-          <TotalBalance
-            totalEarnedUsd={totalEarnedUsd}
-            toWithdrawUsd={toWithdrawUsd}
-          />
-          <ToWithdrawList
+          <TotalBalance currencies={currencies} ethUsd={ethUsd} />
+          {/*<ToWithdrawList
             toWithdrawEth={toWithdrawEth}
             toWithdrawUsd={toWithdrawUsd}
             slicers={slicers}
             account={account}
-          />
+          /> */}
         </main>
       </ConnectBlock>
     </Container>
