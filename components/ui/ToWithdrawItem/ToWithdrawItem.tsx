ce961b8f9dfb7959a0ae1fc9f8fa6aa147179417
Dom-Mac <domenicochad@gmail.com>
2022-08-12 14:33:16 +0200

    refactor useTokenMetadata hook

--- components/ui/ToWithdrawItem/ToWithdrawItem.tsx
+++ components/ui/ToWithdrawItem/ToWithdrawItem.tsx
@@ -6,7 +6,8 @@ import { useEffect } from "react"
 
 const ToWithdrawItem = ({ currency }) => {
   const currencyAddress = currency?.id.split("-")[1]
-  const tokenMetadata = useTokenMetadata(currencyAddress)
+  const { tokenName, tokenSymbol, tokenLogo } =
+    useTokenMetadata(currencyAddress)
   console.log(currency)
   const toWithdrawToken = currency?.toWithdraw
   const toWithdrawUsd = currency?.toWithdraw
@@ -19,17 +20,13 @@ const ToWithdrawItem = ({ currency }) => {
           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
         />
         <div className="h-6 mx-2">
-          {tokenMetadata?.logo && (
-            <Image src={tokenMetadata.logo} alt="ETH" width={24} height={24} />
+          {tokenLogo && (
+            <Image src={tokenLogo} alt="ETH" width={24} height={24} />
           )}
         </div>
         <div className="pt-1 text-left">
-          <p className="text-lg font-normal leading-none">
-            {tokenMetadata?.symbol}
-          </p>
-          <p className="text-xs font-normal text-slate-400">
-            {tokenMetadata?.name}
-          </p>
+          <p className="text-lg font-normal leading-none">{tokenSymbol}</p>
+          <p className="text-xs font-normal text-slate-400">{tokenName}</p>
         </div>
       </div>
       <div className="flex items-center">
