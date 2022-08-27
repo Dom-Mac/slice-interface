5f89aee7d786be70bcc822925c7de094b0d8e26e
Dom-Mac <domenicochad@gmail.com>
2022-08-27 22:41:08 +0200

    fix no currencies to withdraw scenario

--- components/ui/ToWithdrawList/ToWithdrawList.tsx
+++ components/ui/ToWithdrawList/ToWithdrawList.tsx
@@ -17,6 +17,9 @@ const ToWithdrawList = ({ currencies, account, setCurrencies }: Props) => {
   const [selectedTokens, setSelectedTokens] = useState([])
   const [success, setSuccess] = useState(false)
   const [logs, setLogs] = useState<LogDescription[]>()
+  const currenciesToWithdraw = currencies.filter(
+    (currency) => currency.toWithdraw > 1
+  )
 
   const handleSelectAll = () => {
     let addresses = []
@@ -42,19 +45,17 @@ const ToWithdrawList = ({ currencies, account, setCurrencies }: Props) => {
   }
 
   const withdrawAction = () => {
-    const currenciesToWithdraw = currencies
-      .filter((currency) => currency.toWithdraw > 1)
-      .map((currency) => {
-        return currency.id.split("-")[1]
-      })
+    const toWithdrawAddresses = currenciesToWithdraw.map((currency) => {
+      return currency.id.split("-")[1]
+    })
 
     // If there is only one currency available, withdraw all or selected is the same thing
-    if (currenciesToWithdraw.length === 1) {
-      return Withdraw(signer, account, currenciesToWithdraw[0])
+    if (toWithdrawAddresses.length === 1) {
+      return Withdraw(signer, account, toWithdrawAddresses[0])
     } else {
       if (selectedTokens.length === 0) {
         // Withdraw all in batch
-        return BatchWithdraw(signer, account, currenciesToWithdraw)
+        return BatchWithdraw(signer, account, toWithdrawAddresses)
       } else if (selectedTokens.length === 1) {
         // Withdraw the one selected
         return Withdraw(signer, account, selectedTokens[0])
@@ -114,44 +115,50 @@ const ToWithdrawList = ({ currencies, account, setCurrencies }: Props) => {
         </p>
         <div>
           {/* TODO: What if message ? */}
-          <BlockchainCall
-            transactionDescription={`Withdraw tokens`}
-            saEventName="withdraw_to_owner"
-            action={() => withdrawAction()}
-            success={success}
-            setSuccess={setSuccess}
-            setLogs={setLogs}
-            confetti={true}
-            label={
+          {currenciesToWithdraw.length ? (
+            <BlockchainCall
+              transactionDescription={`Withdraw tokens`}
+              saEventName="withdraw_to_owner"
+              action={() => withdrawAction()}
+              success={success}
+              setSuccess={setSuccess}
+              setLogs={setLogs}
+              confetti={true}
+              label={
+                <p className="text-sm font-normal border-b border-black dark:border-yellow-400 md:text-base">
+                  {selectedTokens.length > 0
+                    ? "Widthraw selected"
+                    : "Widthraw all"}
+                </p>
+              }
+              isCustomButton={true}
+            />
+          ) : (
+            <div className="py-1 pt-[7px] md:pt-[4px]">
               <p className="text-sm font-normal border-b border-black dark:border-yellow-400 md:text-base">
-                {selectedTokens.length > 0
-                  ? "Widthraw selected"
-                  : "Widthraw all"}
+                Widthraw all
               </p>
-            }
-            isCustomButton={true}
-          />
+            </div>
+          )}
         </div>
       </div>
-      {currencies?.map((currency, index) => {
-        if (currency.toWithdraw > 1) {
-          return (
-            <>
-              <ToWithdrawItem
-                currency={currency}
-                currencies={currencies}
-                setCurrencies={setCurrencies}
-                account={account}
-                signer={signer}
-                isChecked={selectedTokens.includes(currency.id.split("-")[1])}
-                key={index}
-                handleSelected={handleSelected}
-              />
-            </>
-          )
-        }
+      {currenciesToWithdraw?.map((currency, index) => {
+        return (
+          <>
+            <ToWithdrawItem
+              currency={currency}
+              currencies={currencies}
+              setCurrencies={setCurrencies}
+              account={account}
+              signer={signer}
+              isChecked={selectedTokens.includes(currency.id.split("-")[1])}
+              key={index}
+              handleSelected={handleSelected}
+            />
+          </>
+        )
       })}
-      {!currencies.length && <FakeWithdrawItems />}
+      {!currenciesToWithdraw.length && <FakeWithdrawItems />}
     </div>
   )
 }
