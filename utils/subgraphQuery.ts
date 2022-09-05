aeffd1e8bb1e497a9ac86cf20385c43c1a040cec
Dom-Mac <domenicochad@gmail.com>
2022-09-05 23:48:04 +0200

    cleaned subgraph query from dev data

--- utils/subgraphQuery.ts
+++ utils/subgraphQuery.ts
@@ -17,46 +17,7 @@ export const graphQuery = async (
       `
     })
 
-    setData({
-      payee: {
-        __typename: "Payee",
-        currencies: [
-          {
-            __typename: "PayeeCurrency",
-            id: "0x4d5d7d63989bbe6358a3352a2449d59aa5a08267-0x0000000000000000000000000000000000000000",
-            withdrawn: "15794999999999999",
-            toWithdraw: "9750000000000001",
-            toPayToProtocol: "250000000000001",
-            paidToProtocol: "404999999999997"
-          },
-          {
-            __typename: "PayeeCurrency",
-            id: "0x4d5d7d63989bbe6358a3352a2449d59aa5a08267-0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-            withdrawn: "157",
-            toWithdraw: "1542",
-            toPayToProtocol: "250000000000001",
-            paidToProtocol: "404999999999997"
-          },
-          {
-            __typename: "PayeeCurrency",
-            id: "0x4d5d7d63989bbe6358a3352a2449d59aa5a08267-0xdAC17F958D2ee523a2206206994597C13D831ec7",
-            withdrawn: "3670",
-            toWithdraw: "975000",
-            toPayToProtocol: "2500000",
-            paidToProtocol: "404999999999997"
-          },
-          {
-            __typename: "PayeeCurrency",
-            id: "0x4d5d7d63989bbe6358a3352a2449d59aa5a08267-0x6fa5FF63B2752265c6Bd9350591f97A7dAd9e918",
-            withdrawn: "3670",
-            toWithdraw: "975000",
-            toPayToProtocol: "2500000",
-            paidToProtocol: "404999999999997"
-          }
-        ]
-      }
-    })
-    // setData(data)
+    setData(data)
   } catch (err) {
     console.log("Error fetching data: ", err)
   }
