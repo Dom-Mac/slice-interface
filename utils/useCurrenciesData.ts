ab3617942ee7464f9210d0c4c4313fb28c6af035
Dom-Mac <domenicochad@gmail.com>
2022-09-05 22:34:21 +0200

    refactor useCurrenciesData hook to remove unnecessaries computations

--- utils/useCurrenciesData.ts
+++ utils/useCurrenciesData.ts
@@ -68,9 +68,9 @@ const getCurrenciesMetadata = async (currencies, dbCurrencies) => {
     if (dbCurrency) {
       // if present push its Metadata inside the requests
       requests.push({
-        name: dbCurrency?.name || "",
-        symbol: dbCurrency?.symbol || "",
-        logo: dbCurrency?.logo || ""
+        name: dbCurrency.name,
+        symbol: dbCurrency.symbol,
+        logo: dbCurrency.logo
       })
     } else {
       if (address === ethers.constants.AddressZero) {
@@ -95,64 +95,108 @@ const getCurrenciesMetadata = async (currencies, dbCurrencies) => {
   return formattedMetadata
 }
 
-const getQuotes = async (metadata, dbCurrencies, currencies) => {
-  const dateNow = new Date()
-  const minutesBetweenUpdates = 1
-  const toBeUpdated = dbCurrencies.filter(
-    (currency) =>
-      !currency.quote ||
-      Number(currency.updatedAt) <
-        Number(dateNow) - minutesBetweenUpdates * 60000
-  )
-
+const getQuotes = async (metadata, currencies) => {
   const formattedData = {}
-  // check if there is at least one token to be updated, or if one token is not inside database
-  if (toBeUpdated.length || dbCurrencies.length != currencies.length) {
-    const response = await fetcher("/api/getQuotes", {
-      method: "POST",
-      headers: { Accept: "application/json" },
-      body: JSON.stringify({ tokens: metadata })
-    })
-    // console.log(response.data["USDC"][0].platform)
-    Object.keys(response.data).forEach((key) => {
-      formattedData[key] = response.data[key][0]?.quote?.USD?.price
-    })
-  } else {
-    metadata.forEach((token) => {
-      formattedData[token.symbol] = dbCurrencies.find(
-        (currency) => currency.symbol === token.symbol
-      ).quote
-    })
-  }
+
+  const response = await fetcher("/api/getQuotes", {
+    method: "POST",
+    headers: { Accept: "application/json" },
+    body: JSON.stringify({ tokens: metadata })
+  })
+
+  Object.keys(response.data).forEach((key, index) => {
+    const results = response.data[key] // array of currencies with the same key
+    const currencyAddress = metadata?.address
+      ? metadata.address
+      : currencies[index].id.split("-")[1]
+    if (currencyAddress === ethers.constants.AddressZero) {
+      // if it's ETH take the first value of the array
+      formattedData[key] = results[0]?.quote?.USD?.price
+    } else {
+      results.forEach((result) => {
+        // check if the currency found by symbol has the correct address,
+        // otherwise it's not the same currency
+        const price =
+          result.platform?.token_address?.toLowerCase() ===
+          currencyAddress.toLowerCase()
+            ? result.quote?.USD.price
+            : 0
+        formattedData[key] = price
+      })
+    }
+  })
 
   return formattedData
 }
 
 export default function useCurrenciesData(currencies: Currency[]): Currency[] {
+  // Custom hook, takes as param a list of currencies from the subgraph
+  // and ads symbol, name, logo and quote
+
   const [currenciesData, setCurrenciesData] = useState<Currency[]>([])
 
   const getData = async () => {
     const currenciesAddresses = currencies.map((c) => c.id.split("-")[1])
     const dbCurrencies = await getDbCurrencies(currenciesAddresses)
-    const metadata = await getCurrenciesMetadata(currencies, dbCurrencies)
-    const quotes = await getQuotes(metadata, dbCurrencies, currencies)
-    console.log("dbCurrencies", dbCurrencies)
-    console.log("metadata", metadata)
-    console.log("quotes", quotes)
-
     let formattedData: Currency[] = []
-    if (Object.keys(quotes).length && metadata.length) {
-      currencies?.forEach((currency, index) => {
-        const currencyMetadata = metadata[index]
+
+    // Case in which all the requested currencies are known
+    if (dbCurrencies.length === currencies.length) {
+      console.log("Case in which all the requested currencies are known")
+      const dateNow = new Date()
+      const minutesBetweenUpdates = 5
+      // check if there are quotes to be updated
+      const quotesToBeUpdated = dbCurrencies.filter(
+        (currency) =>
+          !currency.quote ||
+          Number(currency.updatedAt) <
+            Number(dateNow) - minutesBetweenUpdates * 60000
+      )
+      let quotes
+      // if there are tokens to be updated, get quotes from coin market cap
+      if (quotesToBeUpdated.length) {
+        console.log(
+          "Case in which all the requested currencies are known and quotes to be updated"
+        )
+        quotes = await getQuotes(dbCurrencies, currencies)
+      }
+
+      // format data
+      currencies?.forEach((currency) => {
+        const dbCurrency = dbCurrencies.find(
+          (dbCurrency) => currency.id.split("-")[1] === dbCurrency.address
+        )
         formattedData.push({
           ...currency,
-          symbol: currencyMetadata.symbol,
-          name: currencyMetadata.name,
-          logo: currencyMetadata.logo,
-          quote: quotes[currencyMetadata?.symbol]
+          symbol: dbCurrency.symbol,
+          name: dbCurrency.name,
+          logo: dbCurrency.logo,
+          // if the quotes have been updated return the new value, else return the db value
+          quote: Object.keys(quotes).length
+            ? quotes[dbCurrency.symbol]
+            : dbCurrency.quote
         })
       })
+    } else {
+      // Case in which one or more currencies are unknown
+      // so are needed metadata from alchemy and quotes from coin market cap
+      const metadata = await getCurrenciesMetadata(currencies, dbCurrencies)
+      const quotes = await getQuotes(metadata, currencies)
+      console.log("Case in which one or more currencies are unknown")
+      if (Object.keys(quotes).length && metadata.length) {
+        currencies?.forEach((currency, index) => {
+          const currencyMetadata = metadata[index]
+          formattedData.push({
+            ...currency,
+            symbol: currencyMetadata.symbol,
+            name: currencyMetadata.name,
+            logo: currencyMetadata.logo,
+            quote: quotes[currencyMetadata?.symbol]
+          })
+        })
+      }
     }
+
     setCurrenciesData(formattedData)
   }
 
